c4cfae6670de2c184dcd77aa09259555e05779e4
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-15 15:46:16 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,9 +21,9 @@ def update_status
    }
 
   
-epic_tragedy[:montague][:hero][:status] = "dead",
-epic_tragedy[:capulet][:heroine][:status] = "dead",
-puts epic_tragedy
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy
 
 
 def add_characters
